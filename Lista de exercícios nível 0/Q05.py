17176dfb2cb06ff8a6ec847f763a730de559c7cf
Amanda Karolline <amandakarolline@hotmail.com>
2022-03-29 23:45:33 -0300

    Lista 0 Questão 05

---
+++ Lista de exercícios nível 0/Q05.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+def media(valor1, valor2, valor3):
+    soma = valor1 + valor2 + valor3
+    media = soma/3
+    return media
+
+
+#
+# Seu teste
+#
+assert 7 == media(6, 7, 8)
