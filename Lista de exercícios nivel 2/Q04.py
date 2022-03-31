0a33cf01079c965d01811ec77aafb6e192e29d06
Amanda Karolline <amandakarolline@hotmail.com>
2022-03-30 23:43:40 -0300

    Lista 2 Questão 04

---
+++ Lista de exercícios nivel 2/Q04.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+#
+# Converte fahrenheit em celsius
+#
+def toCelsius(fahrenheit):
+    c = (fahrenheit - 32) * 5 / 9
+    return c
+
+
+#
+# Converte celsius em fahrenheit
+#
+def toFahrenheit(celsius):
+    f = 32 + 9 * celsius / 5
+    return f
+
+
+#
+# Testes
+#
+celsius    = 100
+fahrenheit = 212
+
+assert celsius == toCelsius(fahrenheit)
+assert fahrenheit == toFahrenheit(celsius)
