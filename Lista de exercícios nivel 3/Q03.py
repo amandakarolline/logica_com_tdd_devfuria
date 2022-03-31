5f1eadbe97a3d8b52c3b2f1f91e804af381c6a27
Amanda Karolline <amandakarolline@hotmail.com>
2022-03-31 14:05:29 -0300

    Lista 3 Questão 03

--- Lista de exercícios nivel 3/Q03.py
+++ Lista de exercícios nivel 3/Q03.py
@@ -6,18 +6,17 @@
 def mdc(num1, num2):
     m = 1
     i = 2
-    while num1 != 1 and num2 != 1 and i < num1 and i < num2:
+    while i <= num1 and i <= num2:
         while num1 % i == 0 and num2 % i == 0:
             m *= i
             num1 /= i
             num2 /= i
         i += 1
-    print(m)
     return m
 
 #
 # Testes
 #
-assert 3 == mdc(24, 9)
+assert 4 == mdc(24, 20)
 assert 10 == mdc(30, 20)
 assert 6 == mdc(18, 60)
